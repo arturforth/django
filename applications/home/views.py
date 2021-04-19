e66e6682b4db7d864800c96707ec29231dde2fd7
metalfanbase <43826270+metalfanbase@users.noreply.github.com>
2021-04-19 12:29:24 -0300

    Se agregan los archivos creados anteriormente

---
+++ applications/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView, ListView
+
+class PruebaView(TemplateView):
+    template_name = 'home/prueba.html'
+
+
+
+
+class PruebaListView(ListView):
+    template_name = 'home/lista.html'
+    context_object_name = 'listaNumeros'
+    queryset = ['0', '10', '20', '30'] 
