204a1ac47267897b4f7cc4eccbab8ac21bc69ef5
metalfanbase <43826270+metalfanbase@users.noreply.github.com>
2021-04-20 20:08:44 -0300

    minor changes

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Representacion de una DB
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50)
+    short_name = models.CharField('Nombre Corto', max_length=20, unique=True)
+    anulate = models.BooleanField('Anulado', default=False) # Campo para especificar si el departamento esta activo o no
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name + ' - ' + self.short_name
