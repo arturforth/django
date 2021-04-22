1ba3161a1fb219f42fd88c470bf8dd04f137ee59
metalfanbase <43826270+metalfanbase@users.noreply.github.com>
2021-04-21 23:00:16 -0300

    cambio de sqlite a postgresql

--- empleados/settings/local.py
+++ empleados/settings/local.py
@@ -11,8 +11,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',
+        'USER': 'arturp',
+        'PASSWORD': '12345654321',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
