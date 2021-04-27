1ca727de029813234e1d0c5c041ddb9cc6e94ef0
metalfanbase <43826270+metalfanbase@users.noreply.github.com>
2021-04-27 20:47:23 -0300

    34 - Personalizacion de tablas en el admin

--- applications/persona/models.py
+++ applications/persona/models.py
@@ -1,9 +1,21 @@
 from django.db import models
-from applications.departamento.models import Departamento 
+from applications.departamento.models import Departamento
+
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
+
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades del empleado'
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.habilidad
+
 
 # Create your models here.
 class Empleado(models.Model):
     # Modelo para tabla empleado
+    
     job_choices = (
         ('0', 'Contadora'),
         ('1', 'Administrador'),
@@ -15,7 +27,14 @@ class Empleado(models.Model):
     last_name = models.CharField('Apellidos', max_length=60)
     jobs = models.CharField('Trabajo', max_length=1, choices=job_choices)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # imagen = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    avatar = models.ImageField(upload_to='empleado', blank=True, null=True)
+    habilidades = models.ManyToManyField(Habilidades)
+
+    class Meta:
+        verbose_name = 'Mi Empleado'
+        verbose_name_plural = 'Empleados de la empresa'
+        ordering = ['first_name', 'last_name']
+        unique_together = ('first_name', 'departamento')
 
     def __str__(self):
         return str(self.id) + ' - ' + self.first_name + ' - ' + self.last_name
