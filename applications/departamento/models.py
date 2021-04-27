422f4992d19589df6aa04e5920390684d779f255
metalfanbase <43826270+metalfanbase@users.noreply.github.com>
2021-04-27 20:09:03 -0300

    Se agreso la clase Meta en la app departamento

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -7,5 +7,11 @@ class Departamento(models.Model):
     short_name = models.CharField('Nombre Corto', max_length=20, unique=True)
     anulate = models.BooleanField('Anulado', default=False) # Campo para especificar si el departamento esta activo o no
 
+    class Meta:
+        verbose_name = 'Mi Departamento'
+        verbose_name_plural = 'Areas de la empresa'
+        ordering = ['id'] # Ordena por orden alfabetico
+        unique_together = ('name', 'short_name')    # No se puede registrar una combinacion de name y short_name igual a otra
+
     def __str__(self):
         return str(self.id) + ' - ' + self.name + ' - ' + self.short_name
